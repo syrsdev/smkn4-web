96b0748c6eabde49598e266057bef6fe2197a45a
Surya <surya.nata.aardhana@gmail.com>
2023-10-18 15:25:35 +0700

    style: navbar

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "@/Components/Navbar/Navbar";
+import { Head } from "@inertiajs/react";
+import React from "react";
+
+function Home() {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    );
+}
+
+export default Home;
