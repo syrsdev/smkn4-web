93fe8f74a0695e53c2b7b7e0a88891297f163919
r-aozora <zytrahidayat11@gmail.com>
2023-10-25 14:38:06 +0700

    fix: mengganti nama class Ekskul

--- app/Models/Ekskul.php
+++ app/Models/Ekskul.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Eskul extends Model
+class Ekskul extends Model
 {
     use HasFactory;
 
