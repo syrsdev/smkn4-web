894d1b9c80420a0d9f07fed3e1b90a93158a8245
Surya <surya.nata.aardhana@gmail.com>
2023-10-25 13:43:35 +0700

    style: footer

---
+++ resources/js/Components/Footer/Footer.jsx
@@ -0,0 +1,28 @@
+import FooterLayout from "@/Layouts/FooterLayout";
+import React from "react";
+
+function Footer() {
+    return (
+        <FooterLayout>
+            <div className="flex flex-col w-full gap-5 xl:w-1/3">
+                <img
+                    src="/images/smkn4.svg"
+                    alt=""
+                    className="object-contain w-5/6 xl:w-9/12"
+                />
+                <p className="w-9/12 text-xl">
+                    Jl. Veteran No. 1 A Babakan, Tangerang Kota Tangerang -
+                    Banten
+                </p>
+            </div>
+            <div className="flex flex-col w-full gap-5 xl:w-2/6">
+                <h4 className="font-bold text-[24px] mt-5">MENU</h4>
+            </div>
+            <div className="flex flex-col w-full gap-5 xl:items-center xl:w-2/6">
+                <h4 className="font-bold text-[24px] mt-5">SOCIAL MEDIA</h4>
+            </div>
+        </FooterLayout>
+    );
+}
+
+export default Footer;
