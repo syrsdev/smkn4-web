38b5230c6a43b167623b6353bf32b4b29e9340fe
r-aozora <zytrahidayat11@gmail.com>
2023-10-31 20:01:50 +0700

    chore: menambahkan file js untuk choices dan tinymce

---
+++ public/assets/js/page/modules-tinymce.js
@@ -0,0 +1,5 @@
+tinymce.init({
+    selector: 'textarea',
+    plugins: 'ai tinycomments mentions anchor autolink charmap code codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss',
+    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+});
