734d30948abe8072a404673fe1779c8ef52ff037
Surya <surya.nata.aardhana@gmail.com>
2023-11-05 18:22:19 +0700

    style: sub navbar & typinf animation hero section

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -3,22 +3,23 @@ import LandingLayout from "@/Layouts/LandingLayout";
 import Container from "@/Components/Container/Container";
 import ButtonPrimary from "@/Components/ButtonPrimary/Button";
 import { FaAngleRight } from "react-icons/fa6";
-
-function Home() {
+import ReactTypingEffect from "react-typing-effect";
+function Home(props) {
+    let namaSekolah = props.sekolah.nama_sekolah;
     return (
-        <LandingLayout>
+        <LandingLayout subnav={props.subNavbar}>
             <Container
-                background={
-                    "flex py-10 lg:items-start text-secondary justify-center flex-col xl:min-h-[530px] bg-fixed bg-top bg-[url('images/hero.png')] bg-no-repeat relative"
+                classname={
+                    "flex py-10 lg:items-start text-secondary justify-center flex-col xl:min-h-[530px] bg-fixed bg-center bg-[url('images/hero.png')] bg-no-repeat relative"
                 }
             >
-                <div className="absolute inset-0 bg-black opacity-40 lg:bg-gradient-to-r from-black via-slate-700 to-slate-300"></div>
+                <div className="absolute inset-0 bg-black opacity-50 lg:bg-gradient-to-r from-black via-slate-700 to-slate-300"></div>
                 <div className="w-full md:w-10/12 lg:w-1/2">
                     <h1 className="relative z-20 flex flex-col gap-1 text-2xl font-bold xl:gap-2 xl:text-4xl text-secondary">
                         <span className="text-base font-normal xl:text-3xl">
                             SELAMAT DATANG DI WEBSITE
                         </span>{" "}
-                        SMKN 4 KOTA TANGERANG
+                        <ReactTypingEffect text={[namaSekolah]} />
                     </h1>
 
                     <p className="relative z-20 my-5 md:my-7">
@@ -28,7 +29,6 @@ function Home() {
                         repudiandae, labore nulla alias ipsa porro modi rerum?
                         Dolor, vero?
                     </p>
-
                     <ButtonPrimary>
                         Lihat Jurusan <FaAngleRight />
                     </ButtonPrimary>
