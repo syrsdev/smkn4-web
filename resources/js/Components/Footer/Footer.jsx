85261b747d714243ea086d265ac1af81957ab8bc
Surya <surya.nata.aardhana@gmail.com>
2023-11-05 18:43:30 +0700

    chore: remove menu section footer

--- resources/js/Components/Footer/Footer.jsx
+++ resources/js/Components/Footer/Footer.jsx
@@ -2,23 +2,17 @@ import FooterLayout from "@/Layouts/FooterLayout";
 import React from "react";
 import Copyright from "../Copyright/Copyright";
 
-function Footer() {
+function Footer({ logo, alamat }) {
     return (
         <>
             <FooterLayout>
                 <div className="flex flex-col w-full gap-5 md:w-4/6 xl:w-1/3">
                     <img
-                        src="/images/smkn4.svg"
+                        src={`/images/${logo}`}
                         alt="logo smkn 4"
                         className="object-contain w-5/6 md:w-2/3 lg:w-9/12"
                     />
-                    <p className="w-9/12 text-xl">
-                        Jl. Veteran No. 1 A Babakan, Tangerang Kota Tangerang -
-                        Banten
-                    </p>
-                </div>
-                <div className="flex flex-col w-full gap-5 lg:w-2/6">
-                    <h4 className="font-bold xl:text-[24px] mt-5">MENU</h4>
+                    <p className="w-9/12 text-xl">{alamat}</p>
                 </div>
                 <div className="flex flex-col w-full gap-5 xl:items-center lg:w-2/6">
                     <h4 className="font-bold xl:text-[24px] mt-5">
