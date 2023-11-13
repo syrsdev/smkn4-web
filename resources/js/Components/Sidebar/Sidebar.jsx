71bcaf0dda9a185483bae502dff05049a10970f2
Surya <surya.nata.aardhana@gmail.com>
2023-11-13 10:01:12 +0700

    style: dropdown sidebar

--- resources/js/Components/Sidebar/Sidebar.jsx
+++ resources/js/Components/Sidebar/Sidebar.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import { IoChevronDown } from "react-icons/io5";
+import React, { useState } from "react";
 import DropLink from "../Dropdown/DropLink";
-import DropText from "../Dropdown/DropText";
+import SideDropDown from "../Dropdown/SideDropDown";
+import SideDropLink from "../Dropdown/SideDropLink";
 
 function Sidebar({ isActive }) {
+    const [dropdown, setDropdown] = useState(false);
+    const [dropdown2, setDropdown2] = useState(false);
     return (
         <div
-            className={`fixed w-9/12 md:w-1/2 min-h-screen bg-[#F2F7FF] z-[60] top-0 duration-700 lg:hidden ${
+            className={`fixed w-4/6 md:w-1/2 min-h-screen bg-[#F2F7FF] z-[60] top-0 duration-700 lg:hidden ${
                 isActive == false ? "-right-[1000px]" : "-right-1"
             }`}
         >
@@ -14,24 +16,34 @@ function Sidebar({ isActive }) {
                 <img
                     src="/images/favicon.png"
                     alt="logo smkn 4"
-                    width={80}
+                    width={60}
                     className="object-contain"
                 />
-                <ul className="flex flex-col w-full gap-6 px-12 pt-8 mt-5">
+                <ul className="w-full h-[500px] md:h-full px-10 pt-8 overflow-y-auto">
                     <DropLink href="#" border="border-b-2">
                         BERANDA
                     </DropLink>
-                    <DropText>
-                        PROFILE SEKOLAH
-                        <IoChevronDown className="text-[24px]" />
-                    </DropText>
+                    <span onClick={() => setDropdown(!dropdown)}>
+                        <SideDropDown title="PROFILE SEKOLAH" active={dropdown}>
+                            <SideDropLink>
+                                <DropLink href="#">TENTANG SEKOLAH</DropLink>
+                                <DropLink href="#">GURU DAN STAF</DropLink>
+                            </SideDropLink>
+                        </SideDropDown>
+                    </span>
                     <DropLink href="#" border="border-b-2">
                         JURUSAN
                     </DropLink>
-                    <DropText>
-                        BERITA
-                        <IoChevronDown className="text-[24px]" />
-                    </DropText>
+                    <span onClick={() => setDropdown2(!dropdown2)}>
+                        <SideDropDown title="BERITA" active={dropdown2}>
+                            <SideDropLink>
+                                <DropLink href="#">ARTIKEL</DropLink>
+                                <DropLink href="#">BERITA TERKINI</DropLink>
+                                <DropLink href="#">EVENT</DropLink>
+                                <DropLink href="#">PRESTASI</DropLink>
+                            </SideDropLink>
+                        </SideDropDown>
+                    </span>
                     <DropLink href="#" border="border-b-2">
                         PROFILE ALUMNI
                     </DropLink>
