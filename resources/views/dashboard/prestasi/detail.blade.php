6d5c813e42fa931a111b81722a83db3ac0d55bda
N-OLD <arnoldarmawann@gmail.com>
2023-11-13 14:22:20 +0700

    crud:prestasi

---
+++ resources/views/dashboard/prestasi/detail.blade.php
@@ -0,0 +1,98 @@
+@extends('layouts.app')
+
+@section('link')
+    <link rel="stylesheet" href="{{ asset('assets/modules/chocolat/dist/css/chocolat.css') }}">
+@endsection
+
+@section('content')
+    <div class="main-content">
+        <section class="section">
+            <div class="section-header">
+                <div class="section-header-back">
+                    <a href="{{ route('dashboard') }}" class="btn btn-icon">
+                        <i class="fas fa-arrow-left"></i>
+                    </a>
+                </div>
+                <h1>{{ $title }}</h1>
+                <div class="section-header-breadcrumb">
+                    <div class="breadcrumb-item active"><a href="{{ route('dashboard') }}">Dashboard</a></div>
+                    <div class="breadcrumb-item">Kesiswaan</div>
+                    <div class="breadcrumb-item">{{ $title }}</div>
+                </div>
+            </div>
+            <div class="section-body">
+                <h2 class="section-title">{{ $title }}</h2>
+                <p class="section-lead">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate, maiores!
+                </p>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>{{$title}}</h4>
+                            </div>
+                            <div class="card-body">
+                                <div class="tickets">
+                                    <div class="ticket-items" id="ticket-items">
+                                        @foreach ($prestasi as $item)
+                                            <a href="{{ route('prestasi.show', $item->slug) }}">
+                                                <div class="ticket-item">
+                                                    <div class="ticket-title">
+                                                        <h4>{{ $item->judul }}</h4>
+                                                    </div>
+                                                    <div class="ticket-desc">
+                                                        <div>{{ $item->penulis->name }}</div>
+                                                        <div class="bullet"></div>
+                                                        <div>{{ $item->created_at->diffForHumans() }}</div>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        @endforeach
+                                    </div>
+                                    <div class="ticket-content">
+                                        <div class="ticket-header">
+                                            <div class="ticket-sender-picture img-shadow">
+                                                <img src="{{ asset('images/default/profile-admin.png') }}" alt="{{ $prestasi->penulis->name }}">
+                                            </div>
+                                            <div class="ticket-detail">
+                                                <div class="ticket-title">
+                                                    <h4>{{ $prestasi->judul }}</h4>
+                                                </div>
+                                                <div class="ticket-info">
+                                                    <div class="font-weight-600">{{ $prestasi->penulis->name }}</div>
+                                                    <div class="bullet"></div>
+                                                    <div class="text-primary font-weight-600">{{ $prestasi->created_at->diffForHumans() }}</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="ticket-description">
+                                            <div class="gallery mb-3">
+                                                <img src="{{ $prestasi->gambar !== 'no-image-43.png' ? url('storage/' . $kategori . '/' . $prestasi->gambar) : url('images/default/' . $prestasi->gambar) }}" alt="{{ $prestasi->judul }}" style="width: 400px;">
+                                            </div>
+
+                                            {!! $prestasi->konten !!}
+
+                                            <div class="ticket-divider"></div>
+                                            <div class="ticket-form">
+                                                <div class="form-group text-right">
+                                                    <a href="{{ route('prestasi.edit', $post->slug) }}" class="btn btn-primary">
+                                                        Edit {{ ucfirst($kategori) }}
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection
+
+@section('script')
+    <script src="{{ asset('assets/modules/summernote/summernote-bs4.js') }}"></script>
+    <script src="{{ asset('assets/modules/chocolat/dist/js/jquery.chocolat.min.js') }}"></script>
+@endsection
---
+++ resources/views/dashboard/prestasi/index.blade.php
@@ -0,0 +1,142 @@
+@extends('layouts.app')
+
+@section('link')
+    <link rel="stylesheet" href="{{ asset('assets/modules/datatables/datatables.min.css') }}">
+    <link rel="stylesheet"
+        href="{{ asset('assets/modules/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/modules/datatables/Select-1.2.4/css/select.bootstrap4.min.css') }}">
+@endsection
+
+@section('content')
+    <div class="main-content">
+        <section class="section">
+            <div class="section-header">
+                <h1>{{ $title }}</h1>
+                <div class="section-header-breadcrumb">
+                    <div class="breadcrumb-item active">
+                        <a href="{{ route('dashboard') }}">Dashboard</a>
+                    </div>
+                    <div class="breadcrumb-item">Kesiswaan</div>
+                    <div class="breadcrumb-item">Prestasi</div>
+                </div>
+            </div>
+            <div class="section-body">
+                <h2 class="section-title">Kelola {{ $title }}</h2>
+                <p class="section-lead">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>{{ $title }}</h4>
+                                <div class="card-header-action">
+                                    <a href="{{ route('prestasi.create') }}" class="btn btn-sm btn-primary">
+                                        <i class="fas fa-plus"></i>
+                                        Tambah Data
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table table-striped" id="table-1">
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Judul</th>
+                                                <th>Kategori</th>
+                                                <th>Pemenang</th>
+                                                <th>Status</th>
+                                                <th>Publish</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($prestasi as $item)
+                                                <tr>
+                                                    <td>{{ $loop->iteration }}</td>
+                                                    <td>{{ $item->judul}}</td>
+                                                    <td>{{ ucfirst($item->kategori) }}</td>
+                                                    <td>{{ $item->pemenang }}</td>
+                                                    <td>
+                                                        <div class="badge badge-{{ $item->slug }} {{ $item->status === 1 ? 'badge-success' : 'badge-warning' }}">
+                                                            {{ $item->status === 1 ? 'Published' : 'Draft' }}
+                                                        </div>
+													</td>
+                                                    <td>
+														<label class="custom-switch mt-1">
+                                                            <input type="checkbox" class="custom-switch-input" data-slug="{{ $item->slug }}" {{ $item->status === 1 ? 'checked' : '' }}>
+                                                            <span class="custom-switch-indicator"></span>
+                                                        </label>
+													</td>
+                                                    <td>
+                                                        <a href="{{ route('prestasi.show', $item->slug) }}"
+                                                            class="btn btn-sm btn-secondary" data-toggle="tooltip"
+                                                            data-placement="top" title="Lihat Prestasi">
+                                                            <i class="fas fa-eye"></i>
+                                                        </a>
+                                                        <a href="{{ route('prestasi.edit', $item->slug) }}"
+                                                            class="btn btn-sm btn-warning" data-toggle="tooltip"
+                                                            data-placement="top" title="Edit Prestasi">
+                                                            <i class="fas fa-edit"></i>
+                                                        </a>
+                                                        <a href="{{ route('prestasi.destroy', $item->slug) }}"
+                                                            class="btn btn-sm btn-danger" data-confirm-delete="true"
+                                                            data-toggle="tooltip" data-placement="top" title="Hapus Prestasi">
+                                                            <i class="fas fa-trash"
+                                                                onclick="event.preventDefault(); this.closest('a').click();"></i>
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection
+
+@section('script')
+    <script src="{{ asset('assets/modules/datatables/datatables.min.js') }}"></script>
+    <script src="{{ asset('assets/modules/datatables/DataTables-1.10.16/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('assets/modules/datatables/Select-1.2.4/js/dataTables.select.min.js') }}"></script>
+    <script src="{{ asset('assets/modules/jquery-ui/jquery-ui.min.js') }}"></script>
+
+    <!-- Page Specific JS File -->
+    <script src="{{ asset('assets/js/page/modules-datatables.js') }}"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('.custom-switch-input').change(function() {
+                let slug = $(this).data('slug');
+                let status = $(this).prop('checked') === true ? 1 : 0;
+
+                $.ajax({
+                    type: 'GET',
+                    url: '/dashboard/post/' + slug + '/status',
+                    data: {
+                        'status': status,
+                    },
+                    success: function(response) {
+                        if (response.status === '1') {
+                            badge = `
+                                <div class="badge badge-success badge-${response.slug}">Published</div>
+                            `;
+                        } else if (response.status === '0') {
+                            badge = `
+                                <div class="badge badge-warning badge-${response.slug}">Draft</div>
+                            `;
+                        }
+
+                        $(`.badge-${response.slug}`).replaceWith(badge);
+                    }
+                });
+            });
+        });
+    </script>
+@endsection
