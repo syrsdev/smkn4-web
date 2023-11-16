6bc752a2b5b3f41e95bf709a712b4053191e215d
r-aozora <zytrahidayat11@gmail.com>
2023-11-16 19:48:54 +0700

    feat: CRUD user

---
+++ app/Http/Controllers/Dashboard/UsersController.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $user = User::orderBy('name')
+            ->withCount(['post', 'prestasi'])
+            ->get();
+
+        return view('dashboard.user.index')
+            ->with([
+                'title' => 'Data User',
+                'active' => 'User',
+                'subActive' => null,
+                'users' => $user,
+            ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('dashboard.user.create')
+            ->with([
+                'title' => 'Tambah User',
+                'active' => 'User',
+                'subActive' => null,
+            ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        Session::flash('name', $request->input('name'));
+        Session::flash('email', $request->input('email'));
+
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'level' => 'required',
+        ]);
+
+        $name = preg_replace('/[^a-z0-9]+/i', ' ', $request->input('name'));
+        $slug = strtolower(str_replace(' ', '-', $name));
+
+        try {
+            User::create([
+                'slug' => $slug,
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+                'password' => Hash::make($request->input('password')),
+                'level' => $request->input('level'),
+            ]);
+
+            toast('User berhasil dibuat!', 'success');
+
+            return redirect()->route('user.index');
+        } catch (\Exception $e) {
+            toast('User gagal dibuat!', 'warning');
+            
+            return redirect()->back();
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(User $user)
+    {
+        return view('dashboard.user.edit')
+            ->with([
+                'title' => 'Edit User',
+                'active' => 'User',
+                'subActive' => null,
+                'user' => $user,
+            ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'level' => 'required',
+        ]);
+
+        $name = preg_replace('/[^a-z0-9]+/i', ' ', $request->input('name'));
+        $slug = strtolower(str_replace(' ', '-', $name));
+
+        try {
+            $user->update([
+                'slug' => $slug,
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+                'level' => $request->input('level'),
+            ]);
+
+            toast('User berhasil diedit!', 'success');
+
+            return redirect()->route('user.index');
+        } catch (\Exception $e) {
+            toast('User gagal diedit!', 'warning');
+            
+            return redirect()->back();
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        toast('User berhasil dihapus.', 'success');
+            
+        return redirect()->back();
+    }
+}
