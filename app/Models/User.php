0061321bbc8e20c4633838b3bcb1c34e09f9a9c8
r-aozora <zytrahidayat11@gmail.com>
2023-11-16 19:48:14 +0700

    chore: menambahkan slug di tabel user

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,42 +12,22 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $fillable = ['slug', 'name', 'email', 'password', 'level'];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $hidden = ['password', 'remember_token'];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+    protected $casts = ['email_verified_at' => 'datetime', 'password' => 'hashed'];
 
     protected $guarded = [];
 
     protected $primaryKey = 'id';
 
     protected $table = 'users';
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 
     public function post()
     {
