f9197570ae3d1f09465f6fcf2f04810ca626748e
r-aozora <zytrahidayat11@gmail.com>
2023-11-17 11:05:34 +0700

    refactor: hapus status di create dan edit

--- resources/views/dashboard/prestasi/create.blade.php
+++ resources/views/dashboard/prestasi/create.blade.php
@@ -83,15 +83,6 @@
                                         </div>
                                     </div>
                                     <div class="form-group row mb-4">
-                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Status</label>
-                                        <div class="col-sm-12 col-md-7">
-                                            <select class="form-control selectric" name="status">
-                                                <option value="0" selected>Draft</option>
-                                                <option value="1">Publish</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div class="form-group row mb-4">
                                         <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
                                         <div class="col-sm-12 col-md-7">
                                             <button type="submit" class="btn btn-primary">Tambah Data</button>
