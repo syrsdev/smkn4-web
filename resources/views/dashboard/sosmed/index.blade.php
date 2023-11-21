241a7f78b0a33dbee964c46fb707d44cbf0cd9e2
r-aozora <zytrahidayat11@gmail.com>
2023-11-22 00:06:57 +0700

    style: menambahkan icon ke media-object

--- resources/views/dashboard/sosmed/index.blade.php
+++ resources/views/dashboard/sosmed/index.blade.php
@@ -1,10 +1,6 @@
 @extends('layouts.app')
 
 @section('link')
-    <link rel="stylesheet" href="{{ asset('assets/modules/datatables/datatables.min.css') }}">
-    <link rel="stylesheet"
-        href="{{ asset('assets/modules/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/modules/datatables/Select-1.2.4/css/select.bootstrap4.min.css') }}">
 @endsection
 
 @section('content')
@@ -22,17 +18,14 @@
             <div class="section-body">
                 <h2 class="section-title">{{ $title }}</h2>
                 <p class="section-lead">
-                    Di halaman ini Anda bisa menambah, mengedit dan menghapus {{ $title }}.
+                    Di halaman ini Anda bisa mengedit {{ $title }}.
                 </p>
                 <div class="row sortable-card">
                     @foreach ($sosmed as $item)
                         <div class="col-12 col-md-6 col-lg-4">
                             <div class="card">
                                 <div class="card-header text-center">
-                                    <h4>
-                                        <i class="fab fa-{{ strtolower($item->name) }} mr-1"></i>
-                                        {{ $item->name }}
-                                    </h4>
+                                    <h4>{{ $item->name }}</h4>
                                     <div class="card-header-action">
                                         <a data-collapse="#{{ $item->name }}-collapse" class="btn btn-icon btn-info" href="#">
                                             <i class="fas fa-minus"></i>
@@ -41,11 +34,17 @@
                                 </div>
                                 <div class="collapse show" id="{{ $item->name }}-collapse">
                                     <div class="card-body">
-                                        @if ($item->url !== null)
-                                            <a href="{{ $item->url }}" target="_blank">{{ $item->url }}</a>
-                                        @else
-                                            -
-                                        @endif
+                                        <div class="media">
+                                            <i class="fab fa-{{ strtolower($item->name) }} mr-3" style="font-size: 50px"></i>
+                                            <div class="media-body">
+                                                <h5 class="mt-0">{{ $item->name }}</h5>
+                                                @if ($item->url !== null)
+                                                    <a href="{{ $item->url }}" target="_blank">{{ $item->url }}</a>
+                                                @else
+                                                    -
+                                                @endif
+                                            </div>
+                                        </div>
                                     </div>
                                     <div class="card-footer">
                                         <button class="btn btn-sm btn-warning btn-edit" data-toggle="modal" data-target="#editSosmed{{ $item->name }}">
@@ -56,8 +55,7 @@
                                 </div>
                             </div>
                         </div>
-                        @endforeach
-                    </div>
+                    @endforeach
                 </div>
             </div>
         </section>
@@ -66,11 +64,4 @@
 @endsection
 
 @section('script')
-    <script src="{{ asset('assets/modules/datatables/datatables.min.js') }}"></script>
-    <script src="{{ asset('assets/modules/datatables/DataTables-1.10.16/js/dataTables.bootstrap4.min.js') }}"></script>
-    <script src="{{ asset('assets/modules/datatables/Select-1.2.4/js/dataTables.select.min.js') }}"></script>
-    <script src="{{ asset('assets/modules/jquery-ui/jquery-ui.min.js') }}"></script>
-
-    <!-- Page Specific JS File -->
-    <script src="{{ asset('assets/js/page/modules-datatables.js') }}"></script>
 @endsection
