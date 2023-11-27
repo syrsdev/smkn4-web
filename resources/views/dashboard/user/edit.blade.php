79be86cfbc9152df14ddce858ef52564ea3c12d8
r-aozora <zytrahidayat11@gmail.com>
2023-11-27 13:31:49 +0700

    style: CRUD user

--- resources/views/dashboard/user/edit.blade.php
+++ resources/views/dashboard/user/edit.blade.php
@@ -1,86 +1,25 @@
-@extends('layouts.app')
-
-@section('link')
-    <link rel="stylesheet" href="{{ asset('assets/modules/jquery-selectric/selectric.css') }}">
-@endsection
-
-@section('content')
-    <div class="main-content">
-        <section class="section">
-            <div class="section-header">
-                <div class="section-header-back">
-                    <a href="{{ route('user.index') }}" class="btn btn-icon">
-                        <i class="fas fa-arrow-left"></i>
-                    </a>
-                </div>
-                <h1>{{ $title }}</h1>
-                <div class="section-header-breadcrumb">
-                    <div class="breadcrumb-item active">
-                        <a href="{{ route('dashboard') }}">Dashboard</a>
-                    </div>
-                    <div class="breadcrumb-item active">
-                        <a href="{{ route('user.index') }}">Kelola User</a>
-                    </div>
-                    <div class="breadcrumb-item">{{ $title }}</div>
-                </div>
-            </div>
-            <div class="section-body">
-                <h2 class="section-title">{{ $title }}</h2>
-                <p class="section-lead">
-                    Di halaman ini Anda {{ $title }}.
-                </p>
-                <div class="row">
-                    <div class="col-12">
-                        <div class="card">
-                            <div class="card-header">
-                                <h4>Edit User</h4>
-                            </div>
-                            <div class="card-body">
-                                <form action="{{ route('user.update', $user->slug) }}" method="post">
-                                    @csrf
-                                    @method('PUT') 
-                                    <div class="form-group row mb-4">
-                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Nama</label>
-                                        <div class="col-sm-12 col-md-7">
-                                            <input type="text" class="form-control" name="name"
-                                                value="{{ $user->name }}" required autofocus>
-                                        </div>
-                                    </div>
-                                    <div class="form-group row mb-4">
-                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Email</label>
-                                        <div class="col-sm-12 col-md-7">
-                                            <input type="email" class="form-control" name="email"
-                                                value="{{ $user->email }}" required>
-                                        </div>
-                                    </div>
-                                    <div class="form-group row mb-4">
-                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Level</label>
-                                        <div class="col-sm-12 col-md-7">
-                                            <select class="form-control selectric" name="level">
-                                                <option value="admin" {{ $user->level === 'admin' ? 'selected' : '' }}>Admin</option>
-                                                <option value="author" {{ $user->level === 'author' ? 'selected' : '' }} >Author</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div class="form-group row mb-4">
-                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
-                                        <div class="col-sm-12 col-md-7">
-                                            <button type="submit" class="btn btn-primary">Edit Data</button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-    </div>
-@endsection
-
-@section('script')
-    <script src="{{ asset('assets/modules/jquery-selectric/jquery.selectric.min.js') }}"></script>
-
-    <!-- Page Specific JS File -->
-    <script src="{{ asset('assets/js/page/features-post-create.js') }}"></script>
-@endsection
+<div class="collapse" id="edit{{ $item->slug }}">
+    <div class="section-title">Edit User</div>
+    <form action="{{ route('user.update', $item->slug) }}" method="post">
+        @csrf
+        @method('PUT')
+        <div class="form-group">
+            <label for="name">Nama</label>
+            <input type="text" class="form-control" id="name" name="name" value="{{ $item->name }}">
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" id="email" name="email" value="{{ $item->email }}">
+        </div>
+        <div class="form-group">
+            <label for="level">Level</label>
+            <select class="form-control selectric" id="level" name="level">
+                <option value="admin" {{ $item->level === 'admin' ? 'selected' : '' }}>Admin</option>
+                <option value="author" {{ $item->level === 'author' ? 'selected' : '' }}>Author</option>
+            </select>
+        </div>
+        <div class="text-right">
+            <button type="submit" class="btn btn-primary">Edit</button>
+        </div>
+    </form>
+</div>
