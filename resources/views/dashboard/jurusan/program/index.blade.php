b65fc90af82491c8ae692ba58fbbf74bbca4f520
N-OLD <arnoldarmawann@gmail.com>
2023-11-29 11:03:28 +0700

    crud:jurusan/program

---
+++ resources/views/dashboard/jurusan/program/index.blade.php
@@ -0,0 +1,80 @@
+@extends('layouts.app')
+
+@section('link')
+<link rel="stylesheet" href="{{ asset('assets/modules/jquery-selectric/selectric.css') }}">
+@endsection
+
+@section('content')
+    <div class="main-content">
+        <section class="section">
+            <div class="section-header">
+                <h1>{{ $title }}</h1>
+                <div class="section-header-button">
+                    <button class="btn btn-primary" data-toggle="modal" data-target="#tambahProgram">
+                        Tambah Data
+                    </button>
+                </div>
+                <div class="section-header-breadcrumb">
+                    <div class="breadcrumb-item active">
+                        <a href="{{ route('dashboard') }}">Dashboard</a>
+                    </div>
+                    <div class="breadcrumb-item">Jurusan</div>
+                    <div class="breadcrumb-item">Program Keahlian</div>
+                </div>
+            </div>
+            <div class="section-body">
+                <h2 class="section-title">Kelola {{ $title }}</h2>
+                <p class="section-lead">
+                    Disini anda dapat mengelola dan melihat {{ $title }}
+                </p>
+                <div class="row">
+                    @foreach ($program as $item)
+                        <div class="col-12 col-md-6">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4>{{ $item->nama }}</h4>
+                                    <div class="card-header-action">
+                                        <a data-collapse="#{{ $item->slug }}-collapse" class="btn btn-icon btn-info" href="#">
+                                            <i class="fas fa-minus"></i>
+                                        </a>
+                                        <button class="btn btn-sm btn-warning btn-edit" data-slug="{{ $item->slug }}" data-toggle="tooltip" title="Edit Program Keahlian">
+                                            <i class="fas fa-pencil-alt"></i>
+                                        </button>
+                                        <a href="{{ route('program.destroy', $item->slug) }}" class="btn btn-sm btn-danger" data-confirm-delete="true" data-toggle="tooltip" title="Hapus Program Keahlian">
+                                            <i class="fas fa-times" onclick="event.preventDefault(); this.closest('a').click();"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="collapse show" id="{{ $item->slug }}-collapse">
+                                    <div class="card-body">
+                                        <div class="media">
+                                            <div class="media-body">
+                                                <h5 class="mt-0">{{ $item->nama }}</h5>
+                                                <P>Bidang Keahlian: {{ $item->bidang->nama }}</P>
+                                                <p>Total Konsentrasi Keahlian: {{ $item->konsentrasi_count }}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </section>
+        @include('dashboard.jurusan.program.create')
+        @include('dashboard.jurusan.program.edit')
+    </div>
+@endsection
+
+@section('script')
+    <script src="{{ asset('assets/modules/jquery-selectric/jquery.selectric.min.js') }}"></script>
+
+    <!-- Page Specific JS File -->
+    <script>
+        $('.btn-edit').click(function() {
+            let slug = $(this).data('slug');
+            $('#editProgram' + slug).modal('show');
+        });
+    </script>    
+@endsection
