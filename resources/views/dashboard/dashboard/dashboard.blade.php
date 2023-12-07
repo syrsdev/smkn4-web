4844f61c5227f7d3baa8673a27675e953623b031
N-OLD <arnoldarmawann@gmail.com>
2023-12-07 14:53:42 +0700

    post:card

--- resources/views/dashboard/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard/dashboard.blade.php
@@ -28,6 +28,44 @@
                         </div>
                     </div>
                 </div>
+                <div class="col-12 col-lg-4">
+                    <div class="card gradient-bottom">
+                        <div class="card-header">
+                            <h4>Post Terbaru</h4>
+                            <div class="card-header-action dropdown">
+                                <a href="#" data-toggle="dropdown" class="btn btn-danger dropdown-toggle">Kategori</a>
+                                <ul class="dropdown-menu dropdown-menu-sm dropdown-menu-right">
+                                    <li class="dropdown-title">Guruf dan Staff</li>
+                                    <li><a href="#" class="dropdown-item">Agenda</a></li>
+                                    <li><a href="#" class="dropdown-item">Artikel</a></li>
+                                    <li><a href="#" class="dropdown-item">Berita</a></li>
+                                    <li><a href="#" class="dropdown-item">Event</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="card-body" id="top-5-scroll">
+                            <ul class="list-unstyled list-unstyled-border">
+                                <li class="media">
+                                    <img class="mr-3 rounded" width="55" src="assets/img/products/product-3-50.png"
+                                        alt="product">
+                                    <div class="media-body">
+                                        <div class="float-right">
+                                            <div class="font-weight-600 text-muted text-small">Event</div>
+                                        </div>
+                                        <div class="media-title">Hari Lahir Pancasila</div>
+                                        <div class="media-title">1 Juni 2023</div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="card-footer pt-3 d-flex justify-content-center">
+                            <div class="budget-price justify-content-center">
+                                <div class="budget-price-square bg-primary" data-width="20"></div>
+                                <div class="budget-price-label">Total Post</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     </div>
