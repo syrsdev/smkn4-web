61c1252914075ddb1e6925042c05af8a8c02a741
Surya <surya.nata.aardhana@gmail.com>
2023-12-11 15:13:15 +0700

    chore: add link to button

--- resources/js/Components/ButtonPrimary/Button.jsx
+++ resources/js/Components/ButtonPrimary/Button.jsx
@@ -1,9 +1,9 @@
 import { Link } from "@inertiajs/react";
 import React from "react";
 
-function ButtonPrimary({ children }) {
+function ButtonPrimary({ children, href = "#" }) {
     return (
-        <Link href="#">
+        <Link href={href}>
             <button className="relative z-20 px-10 py-3 font-bold rounded-md text-secondary bg-primary transition-all duration-400 ease-in-out hover:scale-105 active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full  before:bg-tertiary before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-md hover:before:left-0 overflow-hidden hover:text-primary flex items-center gap-3 ">
                 {children}
             </button>
