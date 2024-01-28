71dbc91d4aa9e8e12a7d9d82e47c5f2663cd82b1
Surya <surya.nata.aardhana@gmail.com>
2024-01-28 12:19:28 +0700

    feat: tahun copyrigth

--- resources/js/Components/Copyright/Copyright.jsx
+++ resources/js/Components/Copyright/Copyright.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 function Copyright() {
+    let tahun = new Date().getFullYear();
     return (
         <div className="flex justify-center py-5 -mt-[1px] text-secondary bg-primary">
             <p className="text-xs md:text-base">
-                © Copyright SMKN 4 Kota Tangerang - Kulikoding 04
+                Copyright © {tahun} KuliKoding04. All rights reserved.
             </p>
         </div>
     );
