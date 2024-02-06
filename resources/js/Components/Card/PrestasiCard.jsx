557f8ff278460eae13006c0a856596f4f8e565e8
Surya <surya.nata.aardhana@gmail.com>
2024-02-06 20:32:53 +0700

    feat: theme prestasi page

--- resources/js/Components/Card/PrestasiCard.jsx
+++ resources/js/Components/Card/PrestasiCard.jsx
@@ -1,7 +1,7 @@
 import { Link } from "@inertiajs/react";
 import React from "react";
 
-function PrestasiCard({ data, padding = false, dataLength }) {
+function PrestasiCard({ data, padding = false, dataLength, theme }) {
     return (
         <Link
             href={`/prestasi/${data.slug}`}
@@ -16,22 +16,36 @@ function PrestasiCard({ data, padding = false, dataLength }) {
                         alt="thumbnail post"
                         className="object-cover thumbnail h-[120px] w-full md:h-[168px] xl:h-[200px]"
                     />
-                    <div className="absolute top-0 right-0 px-3 py-1 capitalize md:px-4 xl:py-2 xl:px-7 rounded-bl-2xl bg-secondary text-secondary">
+                    <div
+                        style={{
+                            color: `${theme.fontSekunder}`,
+                            backgroundColor: `${theme.sekunder}`,
+                        }}
+                        className="absolute top-0 right-0 px-3 py-1 capitalize md:px-4 xl:py-2 xl:px-7 rounded-bl-2xl"
+                    >
                         {data.kategori}
                     </div>
                 </div>
             </div>
-            <div className={`${padding == true && "px-2 md:px-4 xl:px-6"}`}>
-                <h4 className="text-[16px] xl:text-[20px] font-bold text-primary truncate">
+            <div
+                style={{
+                    color: `${theme.fontPrimer}`,
+                }}
+                className={`${padding == true && "px-2 md:px-4 xl:px-6"}`}
+            >
+                <h4 className="text-[16px] xl:text-[20px] font-bold text-inherit truncate">
                     {data.judul}
                 </h4>
-                <p className=" text-[12px] md:text-[14px] font-semibold text-primary flex items-center gap-1 flex-wrap xl:gap-2">
+                <p className=" text-[12px] md:text-[14px] font-semibold text-inherit flex items-center gap-1 flex-wrap xl:gap-2">
                     <span>Post by {data.penulis.name}</span>
                     {new Date(data.created_at).toLocaleDateString("id-ID")}
                 </p>
             </div>
             <div
-                className={`text-[14px] text-primary underline underline-offset-8 pb-7 mt-auto ${
+                style={{
+                    color: `${theme.fontPrimer}`,
+                }}
+                className={`text-[14px] underline underline-offset-8 pb-7 mt-auto ${
                     padding == true && "px-2 md:px-4 xl:px-6"
                 }`}
                 href={`/prestasi/${data.slug}`}
