dfaf7bca7d9f6d05c90ee363774881ca277cf8f1
N-OLD <arnoldarmawann@gmail.com>
2024-02-08 22:09:39 +0700

    fix:back-button

--- resources/views/dashboard/prestasi/detail.blade.php
+++ resources/views/dashboard/prestasi/detail.blade.php
@@ -9,13 +9,15 @@
         <section class="section">
             <div class="section-header">
                 <div class="section-header-back">
-                    <a href="{{ route('dashboard') }}" class="btn btn-icon">
+                    <a href="{{ route('prestasi.index') }}" class="btn btn-icon">
                         <i class="fas fa-arrow-left"></i>
                     </a>
                 </div>
                 <h1>{{ $title }}</h1>
                 <div class="section-header-breadcrumb">
-                    <div class="breadcrumb-item active"><a href="{{ route('dashboard') }}">Dashboard</a></div>
+                    <div class="breadcrumb-item active">
+                        <a href="{{ route('dashboard') }}">Dashboard</a>
+                    </div>
                     <div class="breadcrumb-item">Kesiswaan</div>
                     <div class="breadcrumb-item">{{ $title }}</div>
                 </div>
