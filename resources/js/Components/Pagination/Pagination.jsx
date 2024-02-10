2897651bd1c239d8a2bff254377e09c170cc5647
Surya <surya.nata.aardhana@gmail.com>
2024-02-10 19:17:20 +0700

    fix:conditional rendering

--- resources/js/Components/Pagination/Pagination.jsx
+++ resources/js/Components/Pagination/Pagination.jsx
@@ -24,108 +24,113 @@ function Pagination({
     return (
         <>
             {lastPage > 1 && (
-                <div
-                    style={{ color: `${theme.fontPrimer}` }}
-                    className="flex items-center justify-center mt-10 cursor-pointer md:mt-16"
-                >
-                    {currentPage > 1 && (
-                        <>
-                            <Link
-                                href={
-                                    window.location.href.slice(-5) == "#post"
-                                        ? `${url.slice(0, -5)}&${
-                                              firstPageUrl.split("?")[1]
-                                          }#post`
-                                        : `${firstPageUrl}#paginate`
-                                }
-                            >
-                                <MdKeyboardDoubleArrowLeft className="text-3xl" />
-                            </Link>
-                            <Link
-                                href={
-                                    window.location.href.slice(-5) == "#post"
-                                        ? `${url.slice(0, -5)}&${
-                                              prevPage.split("?")[1]
-                                          }#post`
-                                        : `${prevPage}#paginate`
-                                }
-                            >
-                                <MdKeyboardArrowLeft className="text-3xl" />
-                            </Link>
-                        </>
-                    )}
-
-                    <div className="flex items-center gap-5 mx-3 text-lg ">
-                        {links
-                            .slice(1, -1)
-                            .filter(
-                                (page) =>
-                                    page.label !== "..." &&
-                                    Math.abs(
-                                        currentPage - parseInt(page.label)
-                                    ) <= screen
-                            )
-                            .map((page, index) => (
+                <>
+                    <div
+                        style={{ color: `${theme.fontPrimer}` }}
+                        className="flex items-center justify-center mt-10 cursor-pointer md:mt-16"
+                    >
+                        {currentPage > 1 && (
+                            <>
                                 <Link
-                                    key={index}
                                     href={
-                                        window.location.href.slice(-5) ===
+                                        window.location.href.slice(-5) ==
                                         "#post"
-                                            ? `${url.slice(0, -5)}&page=${
-                                                  page.label
+                                            ? `${url.slice(0, -5)}&${
+                                                  firstPageUrl.split("?")[1]
                                               }#post`
-                                            : `${page.url}#paginate`
-                                    }
-                                    className={
-                                        currentPage === parseInt(page.label)
-                                            ? `paginasi rounded-full  px-3 ${
-                                                  currentPage >= 10
-                                                      ? "py-2"
-                                                      : "py-1"
-                                              } border-2`
-                                            : "paginasi-false font-bold"
+                                            : `${firstPageUrl}#paginate`
                                     }
                                 >
-                                    {page.label}
+                                    <MdKeyboardDoubleArrowLeft className="text-3xl" />
                                 </Link>
-                            ))}
+                                <Link
+                                    href={
+                                        window.location.href.slice(-5) ==
+                                        "#post"
+                                            ? `${url.slice(0, -5)}&${
+                                                  prevPage.split("?")[1]
+                                              }#post`
+                                            : `${prevPage}#paginate`
+                                    }
+                                >
+                                    <MdKeyboardArrowLeft className="text-3xl" />
+                                </Link>
+                            </>
+                        )}
+
+                        <div className="flex items-center gap-5 mx-3 text-lg ">
+                            {links
+                                .slice(1, -1)
+                                .filter(
+                                    (page) =>
+                                        page.label !== "..." &&
+                                        Math.abs(
+                                            currentPage - parseInt(page.label)
+                                        ) <= screen
+                                )
+                                .map((page, index) => (
+                                    <Link
+                                        key={index}
+                                        href={
+                                            window.location.href.slice(-5) ===
+                                            "#post"
+                                                ? `${url.slice(0, -5)}&page=${
+                                                      page.label
+                                                  }#post`
+                                                : `${page.url}#paginate`
+                                        }
+                                        className={
+                                            currentPage === parseInt(page.label)
+                                                ? `paginasi rounded-full  px-3 ${
+                                                      currentPage >= 10
+                                                          ? "py-2"
+                                                          : "py-1"
+                                                  } border-2`
+                                                : "paginasi-false font-bold"
+                                        }
+                                    >
+                                        {page.label}
+                                    </Link>
+                                ))}
+                        </div>
+
+                        {lastPage != currentPage && (
+                            <>
+                                <Link
+                                    href={
+                                        window.location.href.slice(-5) ==
+                                        "#post"
+                                            ? `${url.slice(0, -5)}&${
+                                                  nextPage.split("?")[1]
+                                              }#post`
+                                            : `${nextPage}#paginate`
+                                    }
+                                >
+                                    <MdKeyboardArrowRight className="text-3xl" />
+                                </Link>
+                                <Link
+                                    href={
+                                        window.location.href.slice(-5) ==
+                                        "#post"
+                                            ? `${url.slice(0, -5)}&${
+                                                  lastPageUrl.split("?")[1]
+                                              }#post`
+                                            : `${lastPageUrl}#paginate`
+                                    }
+                                >
+                                    <MdKeyboardDoubleArrowRight className="text-3xl" />
+                                </Link>
+                            </>
+                        )}
                     </div>
-
-                    {lastPage != currentPage && (
-                        <>
-                            <Link
-                                href={
-                                    window.location.href.slice(-5) == "#post"
-                                        ? `${url.slice(0, -5)}&${
-                                              nextPage.split("?")[1]
-                                          }#post`
-                                        : `${nextPage}#paginate`
-                                }
-                            >
-                                <MdKeyboardArrowRight className="text-3xl" />
-                            </Link>
-                            <Link
-                                href={
-                                    window.location.href.slice(-5) == "#post"
-                                        ? `${url.slice(0, -5)}&${
-                                              lastPageUrl.split("?")[1]
-                                          }#post`
-                                        : `${lastPageUrl}#paginate`
-                                }
-                            >
-                                <MdKeyboardDoubleArrowRight className="text-3xl" />
-                            </Link>
-                        </>
-                    )}
-                </div>
+                    <p
+                        style={{ color: `${theme.fontPrimer}` }}
+                        className="mt-6 font-bold text-center"
+                    >
+                        Page {currentPage} of {lastPage}
+                    </p>
+                </>
             )}
-
-            <p
-                style={{ color: `${theme.fontPrimer}` }}
-                className="mt-6 font-bold text-center"
-            >
-                Page {currentPage} of {lastPage}
-            </p>
         </>
     );
 }
